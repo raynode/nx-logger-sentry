19c8af6cae0135add66723dc9fb1dedb0fac91d3
Tobias Kopelke <tobias.kopelke@liquidlabs.de>
2018-04-04 18:09:26 +0200

    fix(typescript): exported missing interface

--- lib/log-sentry.ts
+++ lib/log-sentry.ts
@@ -1,8 +1,8 @@
 
-import { formatMessage, TransportFn } from '@raynode/nx-logger'
+import { Config, formatMessage, TransportFn } from '@raynode/nx-logger'
 import { createClient, Instance, Options, useInstanceOrCreateClient } from './sentry-client'
 
-export { Options, Instance }
+export { Config, Instance, Options }
 
 export const createTransport = (options: string | Options | Instance): TransportFn => {
   const client = useInstanceOrCreateClient(options)
